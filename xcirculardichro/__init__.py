9f6fb57f328a4e4939a58bde54e156127878545d
John Hammonds <JPHammonds@anl.gov>
2017-08-31 10:41:27 -0500

    Moved base configuration of the logger from the main class into the package definition.

---
+++ xcirculardichro/__init__.py
@@ -0,0 +1,41 @@
+import logging
+import logging.config
+import os
+import traceback
+from configparser import NoSectionError
+
+LOGGER_NAME="xcirculardichro"
+LOGGER_DEFAULT = {
+    'version' : 1,
+    'handlers' : {'consoleHandler' : {'class' : 'logging.StreamHandler',
+                               'level' : 'INFO',
+                               'formatter' : 'consoleFormat',
+                               'stream' : 'ext://sys.stdout'} ,
+                  },
+    'formatters' : {'consoleFormat' : {'format' : '%(asctime)-15s - %(name)s - %(funcName)s- %(levelname)s - %(message)s'},
+                    },
+    'loggers' : {'root' :{'level' : 'INFO',
+                        'handlers' : ['consoleHandler',],
+                      },
+               LOGGER_NAME : {'level' : 'INFO',
+                            'handlers' : ['consoleHandler',],
+                            'qualname' : LOGGER_NAME
+                            }
+               },
+   }
+
+userDir = os.path.expanduser("~")
+logConfigFile = os.path.join(userDir, LOGGER_NAME + 'Log.config')
+if os.path.exists(logConfigFile):
+    print ("logConfigFile " + logConfigFile )
+    try:
+        logging.config.fileConfig(logConfigFile, )
+    except (NoSectionError,TypeError):
+        print ("In Exception to load dictConfig package %s" % LOGGER_NAME)
+        logging.config.dictConfig(LOGGER_DEFAULT)
+    except KeyError as ex:
+        print ("logfile %s was missing or had errant sections %s" %(logConfigFile, ex.args))
+else:
+    logging.config.dictConfig(LOGGER_DEFAULT)
+        
+logger = logging.getLogger(LOGGER_NAME)
