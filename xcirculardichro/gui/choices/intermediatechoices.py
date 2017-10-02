0900ce39c7e39fdd0c05d96e840592dd85ed8073
John Hammonds <JPHammonds@anl.gov>
2017-10-01 22:02:15 -0500

    New method for intermediate data

---
+++ xcirculardichro/gui/choices/intermediatechoices.py
@@ -0,0 +1,11 @@
+'''
+ Copyright (c) 2017, UChicago Argonne, LLC
+ See LICENSE file.
+'''
+from xcirculardichro.gui.choices.abstractchoices import AbstractChoices
+
+
+class IntermediateChoices(AbstractChoices):
+    
+    def __init__(self, parent=None):
+        super(IntermediateChoices, self).__init__(parent=parent)
