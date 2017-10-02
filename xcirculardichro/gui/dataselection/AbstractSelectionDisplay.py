3eb6d1a243962e23fb8ae2864bd9da197bc0a109
John Hammonds <JPHammonds@anl.gov>
2017-10-01 22:38:42 -0500

    Add methods to enable left & right data point selection. Add getNodeContainingScan method remove getDataLabels method add plotOptionChanged signal

--- xcirculardichro/gui/dataselection/AbstractSelectionDisplay.py
+++ xcirculardichro/gui/dataselection/AbstractSelectionDisplay.py
@@ -6,12 +6,15 @@ import logging
 from abc import ABC, abstractmethod, ABCMeta
 from PyQt5 import QtWidgets
 import PyQt5.QtCore as qtCore
-from xcirculardichro.config.loggingConfig import METHOD_ENTER_STR
+from xcirculardichro.config.loggingConfig import METHOD_ENTER_STR,\
+    METHOD_EXIT_STR
+from specguiutils import XMCDException
 logger = logging.getLogger(__name__)
 
 class AbstractSelectionDisplay(QtWidgets.QDialog):
 
-    dataSelectionsChanged = qtCore.pyqtSignal(name="dataSelectionsChanged")    
+    dataSelectionsChanged = qtCore.pyqtSignal(name="dataSelectionsChanged")
+    plotOptionChanged = qtCore.pyqtSignal(name="plotOptionChanged")    
     def __init__(self, parent=None):
         super(AbstractSelectionDisplay, self).__init__(parent=parent)
         self._selectedNodes = []
@@ -29,10 +32,19 @@ class AbstractSelectionDisplay(QtWidgets.QDialog):
     def getPlotAxisLabels(self):
         raise NotImplementedError("Must subclass this and override this method")
         
-    @abstractmethod
-    def getDataLabels(self):
-        raise NotImplementedError("Must subclass this and override this method")
+#     @abstractmethod
+#     def getDataLabels(self):
+#         raise NotImplementedError("Must subclass this and override this method")
 
+    def getNodeContainingScan(self, scan):
+        nodeContainingScan = self._selectedNodes[0]
+        for node in self._selectedNodes:
+            for scanInNode in node.scans:
+                if scanInNode == scan:
+                    nodeContainingScan = node
+        logging.debug(METHOD_EXIT_STR % nodeContainingScan)
+        return nodeContainingScan
+        
     @abstractmethod
     def getPlotAxisLabelsIndex(self):
         raise NotImplementedError("Must subclass this and override this method")
@@ -52,6 +64,14 @@ class AbstractSelectionDisplay(QtWidgets.QDialog):
     def selectedNodes(self):
         return self._selectedNodes
     
+    @abstractmethod
+    def setLeftDataSelection(self, label, selection, average):
+        raise NotImplementedError("Must subclass this and override this method")
+    
+    @abstractmethod
+    def setRightDataSelection(self, label, selection, average):
+        raise NotImplementedError("Must subclass this and override this method")
+    
     def setSelectedNodes(self, selectedNodes):
         logger.debug(METHOD_ENTER_STR % self)
         self._selectedNodes = selectedNodes
@@ -62,4 +82,7 @@ class AbstractSelectionDisplay(QtWidgets.QDialog):
         logger.debug(METHOD_ENTER_STR)
         raise NotImplementedError("Must subclass this and override this method")
         
+class XMCDNoScanSelected(XMCDException):
     
+    def __init__(self, msg):
+        super(XMCDNoScanSelected, self).__init__(msg)
