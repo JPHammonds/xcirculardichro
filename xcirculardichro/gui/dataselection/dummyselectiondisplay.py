86c9559047b8718b7114fee7b915834492fbddd0
John Hammonds <JPHammonds@anl.gov>
2017-11-27 14:53:34 -0600

    Add type for this class so that it can be tested when used Add dummy setPositionerToDisplay method.
    CHange dummy methods

--- xcirculardichro/gui/dataselection/dummyselectiondisplay.py
+++ xcirculardichro/gui/dataselection/dummyselectiondisplay.py
@@ -3,10 +3,13 @@
  See LICENSE file.
 '''
 import logging
-from xcirculardichro.gui.dataselection.AbstractSelectionDisplay import AbstractSelectionDisplay
+from xcirculardichro.gui.dataselection.AbstractSelectionDisplay import AbstractSelectionDisplay,\
+    SelectionTypeNames
 import PyQt5.QtWidgets as qtWidget
+from xcirculardichro import METHOD_ENTER_STR
 logger = logging.getLogger(__name__)
 
+DUMMY_STR = "No action taken here"
 class DummySelectionDisplay(AbstractSelectionDisplay):
     
     def __init__(self,parent=None):
@@ -16,10 +19,24 @@ class DummySelectionDisplay(AbstractSelectionDisplay):
         
         self.addWidget(label)
         self.show()
+        self.selectionType = SelectionTypeNames.DUMMY_SELECTION
         
     def setLeftDataSelection(self, label, selection, average):
-        pass
+        '''
+        Dummy override that does nothing
+        '''
+        logger.debug(METHOD_ENTER_STR % DUMMY_STR)
     
+    def setPositionersToDisplay(self):
+        '''
+        Dummy override that does nothing
+        '''
+        logger.debug(METHOD_ENTER_STR % DUMMY_STR)
+
     def setRightDataSelection(self, label, selection, average):
-        pass
+        '''
+        Dummy override that does nothing
+        '''
+        logger.debug(METHOD_ENTER_STR % DUMMY_STR)
+    
     
