28b3c3ab30008e10a5c7e85a021d9e1e6d7b3951
hammonds <hammonds@ganges.aps.anl.gov>
2018-03-30 14:22:51 -0500

    Rearrange imports in this file to get the order right

--- xcirculardichro/data/__init__.py
+++ xcirculardichro/data/__init__.py
@@ -1,10 +1,19 @@
+from enum import Enum
 # Make definitions to simplify the import process.  Eliminates the need
 # to directly reference the module name when referring to a class
 # makes this more Java like.
+SELECTED_NODES = 'selectedNodes'
+DATA_SELECTION = 'dataSelection' 
+
+class DataSelectionTypes(Enum):
+    RAW, AVERAGED, STEP_NORMALIZED, FULL_NORMALIZED = range(4)
+
 from .datanode import DataNode
 from .filedatanode import FileDataNode
-from .intermediatedatanode import IntermediateDataNode
-from .intermediatescannode import IntermediateScanNode
 from .scandatanode import ScanDataNode
-from .specfiledatanode import SpecFileDataNode
+from .intermediatescannode import IntermediateScanNode
+from .intermediatedatanode import IntermediateDataNode
 from .specscannode import SpecScanNode
+from .specfiledatanode import SpecFileDataNode
+
+
