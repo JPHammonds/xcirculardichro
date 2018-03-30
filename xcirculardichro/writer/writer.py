c661561a300853756b358ea267ac1a2149e27ec5
hammonds <hammonds@ganges.aps.anl.gov>
2018-03-30 13:17:18 -0500

    Change the documentation some.

--- xcirculardichro/writer/writer.py
+++ xcirculardichro/writer/writer.py
@@ -3,7 +3,9 @@
  See LICENSE file.
 '''
 from abc import abstractmethod, ABCMeta
-
+import logging
+from xcirculardichro import METHOD_ENTER_STR
+logger = logging.getLogger(__name__)
 
 class Writer(metaclass=ABCMeta):
     
@@ -11,7 +13,9 @@ class Writer(metaclass=ABCMeta):
     @abstractmethod
     def writeNodes(self, dataNodes):
         '''
-        Write the combined output of severalNodes to the file using this method
+        Write the combined output of severalNodes to the file using 
+        this method
         '''
-        raise NotImplementedError("Must subclass this and override this method")
+        raise NotImplementedError("Must subclass this and override " \
+                                  "this method")
     
