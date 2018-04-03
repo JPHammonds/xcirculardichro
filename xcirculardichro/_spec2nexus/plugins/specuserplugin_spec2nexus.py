07cb920a8b0e6f69ca3cb1637b7f485511053498
hammonds <hammonds@ganges.aps.anl.gov>
2018-04-03 15:44:11 -0500

    Add in handler for #X lines in Spec file.

--- xcirculardichro/_spec2nexus/plugins/specuserplugin_spec2nexus.py
+++ xcirculardichro/_spec2nexus/plugins/specuserplugin_spec2nexus.py
@@ -9,7 +9,7 @@ import logging
 from xcirculardichro import METHOD_ENTER_STR
 logger = logging.getLogger(__name__)
 
-class XCircularDichro(ControlLineHandler):
+class XCircularDichroU(ControlLineHandler):
     '''
     #U
     '''
@@ -37,6 +37,39 @@ class XCircularDichro(ControlLineHandler):
         group = makeGroup(h5parent, 'U', nxclass, description=desc)
         dd = {}
         for item, value in scan.U.items():
+            dd[item] = map(str, value.split())
+        writer.save_dict(group, dd)
+        
+class XCircularDichroX(ControlLineHandler):
+    '''
+    #X
+    '''
+    key = '#X'
+    
+    def process(self, text, spec_obj, *args, **kws):
+        logger.debug(METHOD_ENTER_STR, text)
+        subkey = text.split()[0].lstrip('#')
+        if not hasattr(spec_obj, 'X'):
+            spec_obj.X = {}
+        textLine = strip_first_word(text)
+        items = textLine.split("  ")
+        for item in items:
+            keyValue = item.split(":")
+            try:
+                spec_obj.X[keyValue[0]] = keyValue[1]
+            except IndexError as ex:
+                logger.exception("Error on: " +str(textLine))
+            if isinstance(spec_obj, SpecDataFileHeader):
+                spec_obj.addH5writer(self.key, self.writer)
+            
+            
+    def writer(self, h5parent, writer, scan, nxclass=None, *args, **kwargs):
+        '''Describe how to write X data'''
+        
+        desc = 'Sector4 Experiment Parameters'
+        group = makeGroup(h5parent, 'X', nxclass, description=desc)
+        dd = {}
+        for item, value in scan.X.items():
             dd[item] = map(str, value.split())
         writer.save_dict(group, dd)
         
